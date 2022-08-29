726e9840e192ea3697d92227f9855da5a2068f0e
KIRANKALLA <kallakiran1974@gmail.com>
2022-08-29 14:24:58 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,3 +4,5 @@ start_color= st.select_slider(
      options=[1,2,3,4,5,6,7,8,9,10],
      value=(1, 10))
 st.write('You selected wavelengths between', start_color)
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')

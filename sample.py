20dca7ad80d8f051ae468b22a715c9db6ba6000d
KIRANKALLA <kallakiran1974@gmail.com>
2022-08-29 14:22:14 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import streamlit as st
-start_color, end_color = st.select_slider(
+start_color= st.select_slider(
      'Select a range of color wavelength',
      options=[1,2,3,4,5,6,7,8,9,10],
      value=(1, 10))
-st.write('You selected wavelengths between', start_color, 'and', end_color)
+st.write('You selected wavelengths between', start_color)
